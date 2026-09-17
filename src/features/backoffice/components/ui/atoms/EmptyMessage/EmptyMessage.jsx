0fd6add6da7c738177a84bbe952f37a296f60adb
AidaG91 <aida.garmus@gmail.com>
2026-09-17 10:05:43 +0200

    feat: add extra reset filters button when filters show no options

--- src/features/backoffice/components/ui/atoms/EmptyMessage/EmptyMessage.jsx
+++ src/features/backoffice/components/ui/atoms/EmptyMessage/EmptyMessage.jsx
@@ -2,11 +2,13 @@ import "./EmptyMessage.scss";
 
 export default function EmptyMessage({
   message = "No hay incidencias con esos filtros",
+  action,
   className = "",
 }) {
   return (
-    <p className={["empty-message", className].filter(Boolean).join(" ")}>
-      {message}
-    </p>
+    <div className={["empty-message", className].filter(Boolean).join(" ")}>
+      <p className={["empty-message__text"]}>{message}</p>
+      {action && <div className={["empty-message__action"]}>{action}</div>}
+    </div>
   );
 }
